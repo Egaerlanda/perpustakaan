fc80383cd7a20ca9dbd954d9252e8b70a34e3c94
triauliaaaa <12210488@bsi.ac.id>
2022-10-26 14:22:55 +0700

    migration, model & seeder

---
+++ app/Database/Migrations/2022-10-25-161044_Klasifikasi.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Klasifikasi extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'    => ['type'=>'int', 'constraint'=>10, 'null'=>false, 'unsigned'=>true, 'auto_increment'=>true],
+            'ddc'   => ['type'=>'varchar', 'constraint'=>12, 'null'=>false],
+            'nama'  => ['type'=>'varchar', 'constraint'=>50, 'null'=>false],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('Klasifikasi');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('Klasifikasi');
+    }
+}
---
+++ app/Database/Migrations/2022-10-25-164744_Kategori.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Kategori extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'    => ['type'=>'int', 'constraint'=>10, 'unsigned'=>true, 'auto_increment'=>true],
+            'nama'  => ['type'=>'varchar', 'constraint'=>30, 'null'=>false],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('Kategori');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('Kategori');
+    }
+}
